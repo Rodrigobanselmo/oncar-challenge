14b0e578b9bc93f15f2f212675dbfacc2a081e6b
Rodrigo Barbosa Anselmo <rodrigobanselmo@gmail.com>
2021-10-05 13:54:43 -0300

    feat: created car dto and entity

--- src/modules/cars/dto/create-car.dto.ts
+++ src/modules/cars/dto/create-car.dto.ts
@@ -1,1 +1,44 @@
-export class CreateCarDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsOptional, IsString, MaxLength, MinLength } from 'class-validator';
+
+export class CreateCarDto {
+  @ApiProperty({ description: 'car plate.' })
+  @IsString()
+  @MaxLength(8)
+  @MinLength(7)
+  readonly plate: string;
+
+  @ApiProperty({ description: 'car color.' })
+  @IsString()
+  readonly color: string;
+
+  @ApiProperty({ description: 'car price.' })
+  @IsString()
+  readonly price: string;
+
+  @ApiProperty({ description: 'manufacture year of the car, ex: "2001/2002"' })
+  @MaxLength(9)
+  @IsString()
+  readonly year: string;
+
+  @ApiProperty({ description: 'car mileage in kilometers.' })
+  @IsString()
+  readonly kilometers: string;
+
+  @ApiProperty({ description: 'car fuel type.' })
+  @IsString()
+  readonly fuel: string;
+
+  @ApiProperty({ description: 'car description.' })
+  @IsOptional()
+  @IsString()
+  readonly desc: string;
+
+  @ApiProperty({ description: 'car model id.' })
+  @IsString()
+  readonly modelId: string;
+
+  @ApiProperty({ description: 'car brand id.' })
+  @IsString()
+  readonly brandId: string;
+}
--- src/modules/cars/entities/car.entity.ts
+++ src/modules/cars/entities/car.entity.ts
@@ -1,16 +1,43 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 import { Car } from '.prisma/client';
+
 export class CarEntity implements Car {
+  @ApiProperty()
   id: number;
+
+  @ApiProperty()
   plate: string;
+
+  @ApiProperty()
   color: string;
+
+  @ApiProperty()
   price: string;
+
+  @ApiProperty()
   year: string;
+
+  @ApiProperty()
   kilometers: number;
+
+  @ApiProperty()
   fuel: string;
+
+  @ApiProperty({ required: false, nullable: true })
   desc: string;
+
+  @ApiProperty()
   updatedAt: Date;
+
+  @ApiProperty()
   created_at: Date;
+
+  @ApiProperty()
   modelId: number;
+
+  @ApiProperty()
+  brandId: number;
 
   constructor(partial: Partial<CarEntity>) {
     Object.assign(this, partial);
