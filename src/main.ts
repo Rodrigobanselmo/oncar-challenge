8132eecfce6968d198909c6a9b55f7c19e291fa3
Rodrigo Barbosa Anselmo <rodrigobanselmo@gmail.com>
2021-10-05 09:34:15 -0300

    docs: adding swagger configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('Refresh Tokens')
+    .setDescription(
+      'Full authentication Rest API with typeorm and NestJs using refresh token strategy ',
+    )
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
