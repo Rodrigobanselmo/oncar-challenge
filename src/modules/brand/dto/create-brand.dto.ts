eed4dea73be6e40f56c073d6cf87202aca3a6392
Rodrigo Barbosa Anselmo <rodrigobanselmo@gmail.com>
2021-10-05 11:16:22 -0300

    feat: create brand entity and dto and adding validation pipe

--- src/modules/brand/dto/create-brand.dto.ts
+++ src/modules/brand/dto/create-brand.dto.ts
@@ -1,1 +1,9 @@
-export class CreateBrandDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, MaxLength } from 'class-validator';
+
+export class CreateBrandDto {
+  @ApiProperty({ description: 'car brand.' })
+  @IsString()
+  @MaxLength(50)
+  readonly brand: string;
+}
--- src/modules/brand/entities/brand.entity.ts
+++ src/modules/brand/entities/brand.entity.ts
@@ -1,1 +1,17 @@
-export class Brand {}
+import { Brand } from '.prisma/client';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class BrandEntity implements Brand {
+  @ApiProperty()
+  id: number;
+
+  @ApiProperty()
+  brand: string;
+
+  @ApiProperty()
+  created_at: Date;
+
+  constructor(partial: Partial<BrandEntity>) {
+    Object.assign(this, partial);
+  }
+}
