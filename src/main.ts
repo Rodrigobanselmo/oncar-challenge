d02ea8f7dcdb6cb97dd35e8e8a85af44e2156a81
Rodrigo Barbosa Anselmo <rodrigobanselmo@gmail.com>
2021-10-05 11:05:38 -0300

    feat: adding global validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
@@ -5,12 +6,19 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
 
   const options = new DocumentBuilder()
-    .setTitle('Refresh Tokens')
-    .setDescription(
-      'Full authentication Rest API with typeorm and NestJs using refresh token strategy ',
-    )
+    .setTitle('Oncar API')
+    .setDescription('Oncar challenge Rest API with Prisma and NestJs')
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, options);
