94cf91c966cbcec47e4d809670ab44d4b09266f3
Rodrigo Barbosa Anselmo <rodrigobanselmo@gmail.com>
2021-10-06 20:51:08 -0300

    feat: simulation entity created

--- src/modules/simulations/entities/simulation.entity.ts
+++ src/modules/simulations/entities/simulation.entity.ts
@@ -1,1 +1,44 @@
-export class Simulation {}
+import { ApiProperty } from '@nestjs/swagger';
+import { Address, Simulations } from '.prisma/client';
+
+export class SimulationEntity implements Simulations {
+  @ApiProperty()
+  id: number;
+
+  @ApiProperty()
+  name: string;
+
+  @ApiProperty()
+  cpf: number;
+
+  @ApiProperty()
+  phone: number;
+
+  @ApiProperty()
+  birthDate: string;
+
+  @ApiProperty()
+  email: number;
+
+  @ApiProperty()
+  income: number;
+
+  @ApiProperty()
+  haveHelp: boolean;
+
+  @ApiProperty()
+  dirtyName: boolean;
+
+  @ApiProperty()
+  updated_at: Date;
+
+  @ApiProperty()
+  created_at: Date;
+
+  @ApiProperty()
+  address?: Address;
+
+  constructor(partial: Partial<SimulationEntity>) {
+    Object.assign(this, partial);
+  }
+}
