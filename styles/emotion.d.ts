b9f1209eae78056a8498cf9527bf465042e81746
Rodrigo Barbosa Anselmo <rodrigobanselmo@gmail.com>
2021-10-07 18:24:38 -0300

    feat: conbine chakra and emtion theme to custom theme

---
+++ styles/emotion.d.ts
@@ -0,0 +1,15 @@
+import "@emotion/react"; //Theme;
+import { theme } from "@chakra-ui/react";
+import { themeModel } from "./theme";
+
+type CustomTheme = typeof themeModel.colors;
+type ChakraTheme = typeof theme.colors;
+
+interface NewTheme extends ChakraTheme {}
+interface NewTheme extends CustomTheme {}
+
+declare module "@emotion/react" {
+  export interface Theme {
+    colors: NewTheme;
+  }
+}
