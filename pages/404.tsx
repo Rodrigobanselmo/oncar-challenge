4085131b267f784993da4ffcc0a9c378f93c3dfc
Rodrigo Barbosa Anselmo <rodrigobanselmo@gmail.com>
2021-10-07 18:06:04 -0300

    feat: adding chakra  ui along with 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,9 @@
+import { Center } from "@chakra-ui/react";
+
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return <Center h="100%">404 - Page Not Found</Center>;
+};
+
+export default Home;
