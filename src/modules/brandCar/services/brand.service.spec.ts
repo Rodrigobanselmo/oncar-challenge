9abce1b070c55bc2a3c3cb8a849b5c938e664656
Rodrigo Barbosa Anselmo <rodrigobanselmo@gmail.com>
2021-10-07 11:43:11 -0300

    test: create brand

--- src/modules/brandCar/services/brand.service.spec.ts
+++ src/modules/brandCar/services/brand.service.spec.ts
@@ -1,3 +1,4 @@
+import { BadRequestException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { FakerBrand } from '../../../../test/fake/brand.fake';
 
@@ -21,12 +22,24 @@ describe('BrandService', () => {
     expect(service).toBeDefined();
   });
 
-  describe('Create user', () => {
+  describe('Create brand', () => {
     it('should create and return the brand', async () => {
       const creationData = new FakerBrand();
 
       const brand = await service.create(creationData);
       expect(brand).toHaveProperty('name');
+    });
+
+    it('should not create if brand name already exists', async () => {
+      const creationData1 = new FakerBrand();
+      const creationData2 = new FakerBrand();
+
+      await service.create(creationData1);
+      try {
+        await service.create(creationData2);
+      } catch (err) {
+        expect(err).toBeInstanceOf(BadRequestException);
+      }
     });
   });
 });
