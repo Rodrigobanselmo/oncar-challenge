dbe6f08d2f246bfd429a2b447480247ca201f7d3
Rodrigo Barbosa Anselmo <rodrigobanselmo@gmail.com>
2021-10-07 13:50:20 -0300

    fix: adding related entityes on brand

--- src/modules/brandCar/entities/brand.entity.ts
+++ src/modules/brandCar/entities/brand.entity.ts
@@ -1,5 +1,8 @@
-import { Brand } from '.prisma/client';
 import { ApiProperty } from '@nestjs/swagger';
+import { CarEntity } from 'src/modules/cars/entities/car.entity';
+import { ModelEntity } from 'src/modules/modelCar/entities/model.entity';
+
+import { Brand } from '.prisma/client';
 
 export class BrandEntity implements Brand {
   @ApiProperty()
@@ -7,6 +10,12 @@ export class BrandEntity implements Brand {
 
   @ApiProperty()
   created_at: Date;
+
+  @ApiProperty()
+  models?: ModelEntity[];
+
+  @ApiProperty()
+  cars?: CarEntity[];
 
   constructor(partial: Partial<BrandEntity>) {
     Object.assign(this, partial);
