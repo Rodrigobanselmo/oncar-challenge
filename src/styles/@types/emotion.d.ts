cf0bbac558b2f3758f26934611033781269e3197
Rodrigo Barbosa Anselmo <rodrigobanselmo@gmail.com>
2021-10-09 16:05:35 -0300

    refactor: emotion.d.ts folder

--- src/styles/@types/emotion.d.ts
+++ src/styles/@types/emotion.d.ts
@@ -2,7 +2,7 @@ import "@emotion/react";
 
 import { theme } from "@chakra-ui/react";
 
-import { themeModel } from "./theme";
+import { themeModel } from "../theme";
 
 type CustomTheme = typeof themeModel.colors;
 type ChakraTheme = typeof theme.colors;
