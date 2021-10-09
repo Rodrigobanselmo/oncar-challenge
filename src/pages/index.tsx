91e97b20198ad1c7a63bd91adb11c86b9ff1ee3a
Rodrigo Barbosa Anselmo <rodrigobanselmo@gmail.com>
2021-10-09 16:04:32 -0300

    feat:  services hook to get cars added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,20 @@
-import { Flex, HStack, Select } from "@chakra-ui/react";
-import type { NextPage } from "next";
+import { Box, SimpleGrid, useBreakpointValue } from "@chakra-ui/react";
 import Head from "next/head";
-import Image from "next/image";
-import { ChangeEvent, useEffect, useState } from "react";
-import { Header } from "../components/shared/Header";
-import { useBrandModel } from "../services/hooks/useBrandModel";
-import { Model } from "../@types/models";
+import React from "react";
+
+import { CarCard } from "../components/shared/Cards/Car";
+import { CarSkeleton } from "../components/shared/Cards/CarSkeleton";
 import { CarFilter } from "../components/shared/Filters/CarFilter";
+import { Pagination } from "../components/shared/Pagination";
+import { useCars } from "../services/hooks/useCars";
+
+import type { NextPage } from "next";
 
 const Home: NextPage = () => {
+  const [page, setPage] = React.useState(1);
+  const limit = useBreakpointValue({ sm: 5, md: 6, xl: 10 });
+  const { data, isLoading } = useCars(page, limit);
+
   return (
     <div>
       <Head>
@@ -18,9 +24,29 @@ const Home: NextPage = () => {
           content="financiamento de carros e simulação de crédito"
         />
       </Head>
-      <Flex as="main" justify="center" px={["3rem", null, "4rem"]}>
+
+      <Box
+        as="main"
+        maxW={"container.xl"}
+        m="auto"
+        px={["3rem", null, "4rem"]}
+        pt={20}
+      >
         <CarFilter />
-      </Flex>
+
+        <SimpleGrid pt={20} columns={[1, 1, 1, 1, 2]} gap={"20px 15px"}>
+          {data && data.cars.map((car) => <CarCard key={car.id} car={car} />)}
+          {isLoading &&
+            Array.from(Array(10).keys()).map((i) => <CarSkeleton key={i} />)}
+        </SimpleGrid>
+        <Pagination
+          onPageChange={setPage}
+          totalCountOfRegisters={1000}
+          currentPage={page}
+          registersPerPage={10}
+          siblingsCount={3}
+        />
+      </Box>
       <footer></footer>
     </div>
   );
---
+++ src/services/hooks/useCars/index.ts
@@ -0,0 +1,57 @@
+import { Brand } from "../../../@types/brands";
+import { useQuery, UseQueryOptions, UseQueryResult } from "react-query";
+import { GetResponse, CarsAPI } from "./@interfaces";
+import api from "../../api";
+import { sortAsc } from "../../../utils/sort/asc.sort";
+import { FuelOptions } from "../../../constants/fuel-options.constants";
+
+export async function getCars(
+  page: number,
+  limit: number
+): Promise<GetResponse> {
+  const response = await api.get<CarsAPI>("cars", {
+    params: {
+      page,
+      limit,
+    },
+  });
+
+  await new Promise((res) =>
+    setTimeout(() => {
+      res("");
+    }, 1000)
+  );
+
+  const cars = response.data;
+  const totalCount = Number(response.headers["x-total-count"]);
+  const formatCars = cars.map((car) => {
+    const price = new Intl.NumberFormat("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    }).format(car.price);
+
+    return {
+      ...car,
+      price,
+      fuel: FuelOptions[car.fuel] || "...",
+    };
+  });
+
+  return {
+    cars: formatCars,
+    totalCount,
+  };
+}
+
+export function useCars(
+  page: number,
+  limit = 10,
+  options?: UseQueryOptions
+): UseQueryResult<GetResponse, unknown> {
+  const brands = useQuery(["cars", page, limit], () => getCars(page, limit), {
+    staleTime: 1000 * 60 * 10, // 10 minutes
+    ...(options as any),
+  }) as UseQueryResult<GetResponse, unknown>;
+
+  return brands;
+}
