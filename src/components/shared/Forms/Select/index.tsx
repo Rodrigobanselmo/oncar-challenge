15587abd146fde83aaf8827f573515bc3138f921
Rodrigo Barbosa Anselmo <rodrigobanselmo@gmail.com>
2021-10-10 13:34:36 -0300

    refactor: selector folder created

--- src/components/shared/Forms/Select/index.tsx
+++ src/components/shared/Forms/Select/index.tsx
@@ -1,21 +1,35 @@
-import { Select, SelectProps, useColorModeValue } from "@chakra-ui/react";
+import {
+  Select,
+  SelectProps,
+  FormControl,
+  useColorModeValue,
+  FormLabel,
+} from "@chakra-ui/react";
 import { ReactNode } from "react";
 
 interface ISelectProps extends SelectProps {
   children: ReactNode;
+  label: string;
 }
 
-export function SelectChakra({ children, ...res }: ISelectProps): JSX.Element {
+export function SelectChakra({
+  label,
+  children,
+  ...res
+}: ISelectProps): JSX.Element {
   return (
-    <Select
-      bg={useColorModeValue("white", "gray.700")}
-      boxShadow={useColorModeValue(
-        "0 0.05rem 0.15rem 0 rgba(0, 0, 0, 0.15);",
-        "0 0.125rem 0.25rem 0 rgba(0, 0, 0, 0.35);"
-      )}
-      {...res}
-    >
-      {children}
-    </Select>
+    <FormControl id={`select_${label}`}>
+      <FormLabel>{label}</FormLabel>
+      <Select
+        bg={useColorModeValue("white", "gray.700")}
+        boxShadow={useColorModeValue(
+          "0 0.05rem 0.15rem 0 rgba(0, 0, 0, 0.15);",
+          "0 0.125rem 0.25rem 0 rgba(0, 0, 0, 0.35);"
+        )}
+        {...res}
+      >
+        {children}
+      </Select>
+    </FormControl>
   );
 }
